c9cb3158e6bbcb0fa66f13652a4ddd997f51199e
김동욱 <ehddnr73@naver.com>
2025-09-18 14:34:45 +0900

    프로젝트 생성

---
+++ src/test/java/yeolmok/rsc/RscApplicationTests.java
@@ -0,0 +1,13 @@
+package yeolmok.rsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
